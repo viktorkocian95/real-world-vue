f5b84f6d9a61cce7c0df2c1e363882d4dc440999
Victhor <viktor.kocian95@gmail.com>
2021-08-10 01:02:45 +0200

    L6 - end

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EventList from '../views/EventList.vue'
 import About from '../views/About.vue'
+import EventDetails from '../views/EventDetails.vue'
 
 const routes = [
   {
     path: '/',
     name: 'EventList',
     component: EventList
+  },
+  {
+    path: '/event/:id',
+    name: 'EventDetails',
+    props: true,
+    component: EventDetails
   },
   {
     path: '/about',
